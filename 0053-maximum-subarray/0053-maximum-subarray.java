653df0c339cf18e2d03fd1bedf8dbf4bf619803c
Sathyashree G <sathyashree2027@gmail.com>
2026-09-21 09:11:44 +0530

    Time: 11 ms (1.43%), Space: 77.4 MB (20.22%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,16 +1,29 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int currentSum = 0;
-        int maxSum = Integer.MIN_VALUE;
-        for(int num : nums){
-            currentSum += num;
-            if(currentSum > maxSum){
-                maxSum = currentSum;
-            }
-            if(currentSum < 0){
-                currentSum = 0;
-            }
+        return divide(nums, 0, nums.length - 1);
+    }
+
+    public int divide(int[] nums, int left, int right) {
+        if (left == right) {
+            return nums[left];
         }
-        return maxSum;
+        int mid = left + (right - left) / 2;
+        int leftMax = divide(nums, left, mid);
+        int rightMax = divide(nums, mid + 1, right);
+        int leftSum = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = mid; i >= left; i--) {
+            sum += nums[i];
+            leftSum = Math.max(leftSum, sum);
+        }
+        int rightSum = Integer.MIN_VALUE;
+        sum = 0;
+        for (int i = mid + 1; i <= right; i++) {
+            sum += nums[i];
+            rightSum = Math.max(rightSum, sum);
+        }
+
+        int crossMax = leftSum + rightSum;
+        return Math.max(leftMax, Math.max(rightMax, crossMax));
     }
 }
