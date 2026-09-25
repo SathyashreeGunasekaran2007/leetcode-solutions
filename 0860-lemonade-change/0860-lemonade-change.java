f0230f7b371686755f3b54c71435901ad1356e88
Sathyashree G <sathyashree2027@gmail.com>
2026-09-25 10:37:53 +0530

    Time: 1 ms (99.88%), Space: 72.5 MB (49.59%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int fives = 0;
+        int tens = 0;
+        for(int i : bills){
+            if(i == 5){
+                fives++;
+            }else if(i == 10){
+                if(fives == 0){
+                    return false;
+                }
+                fives--;
+                tens++;
+            }else{
+                if(tens > 0 && fives > 0){
+                    tens--;
+                    fives--;
+                }else if(fives >= 3){
+                    fives -= 3;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
